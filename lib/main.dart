d969eb4da74134e7cd5c5f0a7672bde1229700e6
Brian Grey <mixolyde@gmail.com>
2020-09-09 21:43:19 -0400

    Move main.dart to lib

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'dart:html';
+
+void main() {
+  querySelector('#output').text = 
+    'Your Blaseball Playoff Status app is running.';
+}
--- web/main.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:html';
-
-void main() {
-  querySelector('#output').text = 'Your Blaseball Playoff Status app is running.';
-}
