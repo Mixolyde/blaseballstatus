5956195c8157a4dafd1341c2ab3b564ae3863c6a
Brian Grey <mixolyde@gmail.com>
2020-09-12 22:59:13 -0400

    Skip league pick page, go to standings

--- web/main.dart
+++ web/main.dart
@@ -1,21 +1,26 @@
+import 'dart:async';
 import 'dart:html';
 
 var pickLeagueHTML;
 var standingsHTML;
 
+StreamSubscription leagueLinkSub1;
+StreamSubscription leagueLinkSub2;
+
 void main() {
   getContentPages().then((v) {
     print("Got Content Pages");
-    querySelector('#mncntnt').setInnerHtml(pickLeagueHTML);
-    querySelector('#leagueLink1').onClick.listen(selectLeague1);
-    querySelector('#leagueLink2').onClick.listen(selectLeague2);
+    querySelector('#mncntnt').children.clear();
+    //querySelector('#mncntnt').innerHtml = pickLeagueHTML;
+    //leagueLinkSub1 = querySelector('#leagueLink1').onClick.listen(selectLeague1);
+    //leagueLinkSub2 = querySelector('#leagueLink2').onClick.listen(selectLeague2);
+    querySelector('#mncntnt').innerHtml = standingsHTML;
   });
   
 }
 
 Future<void> getContentPages() async {
   pickLeagueHTML = await HttpRequest.getString('pickLeague.html');
-  //print('Pick League HTML: $pickLeagueHTML');
   standingsHTML = await HttpRequest.getString('standings.html');
 }
 
@@ -24,5 +29,10 @@ void selectLeague2(MouseEvent event) => clickLeague(2);
 
 void clickLeague(int league){
   print("League $league clicked");
-  querySelector('#mncntnt').setInnerHtml(standingsHTML);
+  leagueLinkSub1.cancel();
+  leagueLinkSub2.cancel();
+  print("League subscriptions canceled");
+  querySelector('#mncntnt').children.clear();
+  querySelector('#mncntnt').innerHtml = standingsHTML;
+  print("Standings HTML set");
 }
