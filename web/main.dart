27dc74c83d200e9e8f77dd7c3f25853120f16498
Brian Grey <mixolyde@gmail.com>
2020-09-15 14:19:18 -0400

    Add league data switching to links

--- web/main.dart
+++ web/main.dart
@@ -12,44 +12,13 @@ int activeLeague = 1;
 
 void main() {
   getContentPages().then((v) {
-    print("Got Content Pages");
+    print("Got Content Pages and data");
     //set last update time
     querySelector('#lastUpdate').text = sitedata.lastUpdate;
-    querySelector('#mncntnt').children.clear();
-    querySelector('#mncntnt').innerHtml = standingsHTML;
-    querySelector('#leagueTitle').text = sitedata.sub1name;
     querySelector('#pickLeague1').onClick.listen(selectLeague1);
     querySelector('#pickLeague2').onClick.listen(selectLeague2);
-    TableElement table = querySelector("#standingsTable");
-    sub1Standings.forEach((row){
-      TableRowElement trow = table.addRow();
-      trow.insertCell(0)
-        ..text = row['nickname']
-        ..classes.add('tblteam');
-      trow.insertCell(1)
-        ..text = row['division'];
-      trow.insertCell(2)
-        ..text = row['wins'].toString();
-      trow.insertCell(3)
-        ..text = row['losses'].toString();        
-      trow.insertCell(4)
-        ..text = row['gbLg'].toString();        
-      trow.insertCell(5)
-        ..text = row['gbPo'].toString();        
-      for(int i = 1; i < 6; i++){
-        trow.insertCell(5 + i)
-          ..text = row["po$i"].toString();        
-      }
-        
-    });
-    
-    var sepRow = table.insertRow(6);
-    sepRow.insertCell(0)
-      ..text = '&nbsp;'
-      ..colSpan = 11
-      ..classes.add('sepRow');
+    clickLeague(1);
   });
-  
 }
 
 Future<void> getContentPages() async {
@@ -60,12 +29,50 @@ Future<void> getContentPages() async {
   sub2Standings = json.decode(await HttpRequest.getString('${sitedata.sub2id}.json'));
 }
 
-void selectLeague1(MouseEvent event){
-  print(event);
-  clickLeague(1);
-}
+void selectLeague1(MouseEvent event) => clickLeague(1);
 void selectLeague2(MouseEvent event) => clickLeague(2);
 
 void clickLeague(int league){
   print("League $league clicked");
+  querySelector('#mncntnt').children.clear();
+  querySelector('#mncntnt').innerHtml = standingsHTML;
+  if(league == 1){
+    querySelector('#leagueTitle').text = sitedata.sub1name;
+    populateStandingsTable(sub1Standings);
+  } else {
+    querySelector('#leagueTitle').text = sitedata.sub2name;
+    populateStandingsTable(sub2Standings);
+  }
+}
+
+void populateStandingsTable(List<dynamic> subStandings){
+  TableElement table = querySelector("#standingsTable");
+  subStandings.forEach((row){
+    TableRowElement trow = table.addRow();
+    trow.insertCell(0)
+      ..text = row['nickname']
+      ..classes.add('tblteam');
+    trow.insertCell(1)
+      ..text = row['division'];
+    trow.insertCell(2)
+      ..text = row['wins'].toString();
+    trow.insertCell(3)
+      ..text = row['losses'].toString();        
+    trow.insertCell(4)
+      ..text = row['gbLg'].toString();        
+    trow.insertCell(5)
+      ..text = row['gbPo'].toString();        
+    for(int i = 1; i < 6; i++){
+      trow.insertCell(5 + i)
+        ..text = row["po$i"].toString();        
+    }
+      
+  });
+  
+  var sepRow = table.insertRow(6);
+  sepRow.insertCell(0)
+    ..text = '&nbsp;'
+    ..colSpan = 11
+    ..classes.add('sepRow');  
+  
 }
